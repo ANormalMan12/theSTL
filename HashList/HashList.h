116bce6bcd421d52f47d6311e36d17d25a7016c6
WeiyeWang <wangweiye1205@qq.com>
2022-09-28 09:06:45 +0800

    HashList

--- HashList/HashList.h
+++ HashList/HashList.h
@@ -1,13 +1,19 @@
 #pragma once
 #include<..\LinearList\List.h>
-template<typename dataType> class HashList_Linked{
-    using listType=List<dataType>;
-    private:
-    size_t _size;
-    dataType arr*;
+namespace theSTL{
+    template<typename dataType> class HashList_Linked{
+        using listType=List<dataType>;
+        
+        private:
+        size_t _size;
+        listType arr*;
 
-    public:
-    HashList_Linked(size_t nsize):_size(nsize){
-        arr=new dataType[_size];
-    }
+        public:
+        HashList_Linked(size_t nsize):_size(nsize){
+            arr=new listType[_size];
+        }
+        ~HashList_Linked(){
+            delete[] arr;
+        }
+    };
 }
